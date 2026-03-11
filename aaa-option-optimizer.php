36eba33cb683795b69f6fdcf9c8e8fc9d8d88434
Filip Ilic <ilic.filip@gmail.com>
2026-03-11 10:28:11 +0100

    1.6.1 version bump & changelog

--- aaa-option-optimizer.php
+++ aaa-option-optimizer.php
@@ -7,7 +7,7 @@
  * Plugin Name: AAA Option Optimizer
  * Plugin URI: https://progressplanner.com/plugins/aaa-option-optimizer/
  * Description: Tracks autoloaded options usage and allows the user to optimize them.
- * Version: 1.6.0
+ * Version: 1.6.1
  * License: GPL-3.0+
  * Author: Team Prospress Planner
  * Author URI: https://prospressplanner.com/
